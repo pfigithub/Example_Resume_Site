7378fdad0ce885b385609dd133eda0db8988261d
pfigithub <pampam7072@gmail.com>
2023-01-26 16:48:20 +0330

    Update views.py
    made first view

--- Home_App/views.py
+++ Home_App/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from Home_App.forms import ContactForm
+from django.contrib import messages
 
-# Create your views here.
+
+def index(req):
+    return render(req, 'website/index.html')
+
+def about(req):
+    return render(req, 'website/about.html')
+
+def contact(req):
+    if req.method == 'POST':
+        form = ContactForm(req.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(req, messages.SUCCESS, 'your ticket submitted successfully')
+        else:
+            messages.add_message(req, messages.ERROR, 'your ticket did not submitted')
+    form = ContactForm()
+    return render(req, 'website/contact.html', {'form':form})
+
+
