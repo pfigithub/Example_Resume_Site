ee3a17280e645d8e422898fc6430d7150741e10d
pfigithub <pampam7072@gmail.com>
2023-02-04 11:35:25 +0330

    Create forms.py
    making forms for home_app

---
+++ Home_App/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from Home_App.models import Contact, Newsletter
+from captcha.fields import CaptchaField
+
+class ContactForm(forms.ModelForm):
+    captcha = CaptchaField()
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+
+class NewsletterForm(forms.ModelForm):
+
+    class Meta:
+        model = Newsletter
+        fields = '__all__'
