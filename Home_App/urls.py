0afb3445da5cff90c066b95af022d3419787ccf4
pfigithub <pampam7072@gmail.com>
2023-02-04 11:36:34 +0330

    Update urls.py
    making urls ready for home_app

--- Home_App/urls.py
+++ Home_App/urls.py
@@ -1,10 +1,11 @@
 from Home_App.views import *
 from django.urls import path
 
-app_name = 'website'
+app_name = 'web'
 
 urlpatterns = [
     path('', index, name = 'index'),
     path('about', about, name = 'about'),
-    path('contact', contact, name = 'contact')
+    path('contact', contact, name = 'contact'),
+    path('newsletter', newsletter_view, name = 'newsletter'),
 ]
