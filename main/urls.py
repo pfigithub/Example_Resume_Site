be0b65f675c8c895673b8eae87bbd87dd5a7182f
pfigithub <pampam7072@gmail.com>
2023-02-04 11:38:53 +0330

    Update urls.py
    making urls for main

--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from Home_App.sitemaps import StaticViewSitemap
+from Blog_App.sitemaps import BlogSitemap
+from Accounts.views import Home
+
+sitemaps = {'static': StaticViewSitemap, 'blog': BlogSitemap}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Home_App.urls')),
+    path('blog/', include('Blog_App.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', Home.as_view(), name='home'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', include('robots.urls')),
+    path('captcha/', include('captcha.urls')),
+    path('summernote/', include('django_summernote.urls')),
+    path('__debug__/', include('debug_toolbar.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
