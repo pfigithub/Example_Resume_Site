80190aa11e77b5a47a48431f9fd8772a97514ce7
pfigithub <pampam7072@gmail.com>
2023-02-04 11:35:08 +0330

    Update admin.py
    making admin ready for home_app

--- Home_App/admin.py
+++ Home_App/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from Home_App.models import Contact, Newsletter
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-" 
+    list_display = ('name','email','created_date')  
+    list_filter = ('email',)
+    search_fields = ['name','subject','text']
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Newsletter)
